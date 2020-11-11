536634d9ce621303cf40b583f620249553585789
Schmluk <schmluk@student.ethz.ch>
2020-11-11 12:56:12 +0100

    slim out old ros creation tools

--- panoptic_mapping_ros/src/conversions/ros_component_factory.cpp
+++ panoptic_mapping_ros/src/conversions/ros_component_factory.cpp
@@ -2,26 +2,27 @@
 
 #include <memory>
 #include <string>
-#include <utility>
+
+#include <voxblox_ros/ros_params.h>
 
 #include <panoptic_mapping/3rd_party/config_utilities.hpp>
-
-#include "panoptic_mapping_ros/conversions/ros_params.h"
+#include <panoptic_mapping/integrator/naive_integrator.h>
+#include <panoptic_mapping/integrator/projective_integrator.h>
 
 namespace panoptic_mapping {
 
-std::string ComponentFactoryROS::getType(const ros::NodeHandle& nh) {
-  std::string type;
-  nh.param("type", type, std::string("type param is not set"));
-  return type;
-}
-
 std::unique_ptr<IntegratorBase> ComponentFactoryROS::createIntegrator(
     const ros::NodeHandle& nh) {
-  std::string type = getType(nh);
+  std::string type;
+  nh.param("type", type, std::string("type param is not set"));
   if (type == "naive") {
-    return std::make_unique<NaiveIntegrator>(
-        getNaiveIntegratorConfigFromRos(nh));
+    // Specific implementation for using the standard voxblox integrator.
+        NaiveIntegrator::Config config;
+    config.voxblox_integrator_config =
+        voxblox::getTsdfIntegratorConfigFromRosParam(nh);
+    nh.param("voxblox_integrator_type", config.voxblox_integrator_type,
+             config.voxblox_integrator_type);
+    return std::make_unique<NaiveIntegrator>(config);
   } else if (type == "projective") {
     return std::make_unique<ProjectiveIntegrator>(
         config_utilities::getConfigFromRos<ProjectiveIntegrator::Config>(nh));
