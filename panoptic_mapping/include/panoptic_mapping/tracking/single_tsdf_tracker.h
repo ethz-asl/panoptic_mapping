304c1625e4234aa18860f9a017c19fbb473258e0
Giuliano Albanese <albanesg@student.ethz.ch>
2021-12-26 11:36:11 +0100

    Implement minimal functional single tsdf tracking

--- panoptic_mapping/include/panoptic_mapping/tracking/single_tsdf_tracker.h
+++ panoptic_mapping/include/panoptic_mapping/tracking/single_tsdf_tracker.h
@@ -8,9 +8,13 @@
 #include "panoptic_mapping/3rd_party/config_utilities.hpp"
 #include "panoptic_mapping/labels/label_handler_base.h"
 #include "panoptic_mapping/map/classification/class_layer.h"
+#include "panoptic_mapping/tools/map_renderer.h"
 #include "panoptic_mapping/tracking/id_tracker_base.h"
 
 namespace panoptic_mapping {
+
+class TrackingInfo;
+class TrackingInfoAggregator;
 
 /**
  * @brief Allocates a single submap to emulate running a monolithic TSDF grid as
@@ -27,7 +31,26 @@ class SingleTSDFTracker : public IDTrackerBase {
 
     // System params.
     bool use_detectron = false;
-    bool use_instance_classification = false;
+    bool use_detectron_panoptic = false;
+
+    // Count as valid iso-surface points whose projected depth is within this
+    // distance in meters of the measured depth. Negative values indicate
+    // multiples of the voxel size.
+    float depth_tolerance = -1.0;
+
+    // Accept matches that have at least this value in the computed tracking
+    // metric.
+    float match_acceptance_threshold = 0.5;
+
+    // Subsample the number of looked up vertices when using
+    // 'use_approximate_rendering=false' by this factor squared.
+    int rendering_subsampling = 1;
+
+    // Only initialize a new instance for masks with this many pixels
+    int min_new_instance_size = 1000;
+
+    // Renderer settings. The renderer is only used for visualization purposes.
+    MapRenderer::Config renderer;
 
     Config() { setConfigName("SingleTSDFTracker"); }
 
@@ -47,7 +70,43 @@ class SingleTSDFTracker : public IDTrackerBase {
 
   void parseDetectronClasses(InputData* input);
 
+  /**
+   * @brief Convert ids in the ID image to panoptic ids using Detectron labels
+   *
+   * @param input
+   */
+  void parseDetectronPanopticLabels(InputData* input);
+
+  /**
+   * @brief Render submap for tracking using approximate rendering algorithm.
+   *
+   * @param submap
+   * @param input
+   * @return TrackingInfo
+   */
+  std::vector<TrackingInfo> renderTrackingInfoApproximate(
+      const Submap& submap, const InputData& input);
+
+  /**
+   * @brief Renders the vertex map for tracking.
+   *
+   * Adapted from ProjectiveIDTracker::renderTrackingInfoVertices
+   *
+   * @param map
+   * @param input
+   * @return TrackingInfo
+   */
+  std::vector<TrackingInfo> renderTrackingInfoVertices(const Submap& submap,
+                                                       const InputData& input);
+
+  TrackingInfoAggregator computeTrackingData(SubmapCollection* submaps,
+                                             InputData* input);
+
  private:
+  // A label divisor used to compute a panoptic id from instance and
+  // class id as panoptic_id = class_id * label_divisor + instance_id
+  static constexpr int kPanopticLabelDivisor_ = 1000;
+
   static config_utilities::Factory::RegistrationRos<
       IDTrackerBase, SingleTSDFTracker, std::shared_ptr<Globals>>
       registration_;
@@ -55,6 +114,9 @@ class SingleTSDFTracker : public IDTrackerBase {
 
   int map_id_;
   bool is_setup_ = false;
+
+  MapRenderer renderer_;  // The renderer is only used if visualization is on.
+  cv::Mat rendered_vis_;  // Store visualization data.
 };
 
 }  // namespace panoptic_mapping
--- panoptic_mapping/src/tracking/single_tsdf_tracker.cpp
+++ panoptic_mapping/src/tracking/single_tsdf_tracker.cpp
@@ -5,6 +5,8 @@
 #include <unordered_set>
 #include <utility>
 
+#include "panoptic_mapping/tracking/tracking_info.h"
+
 namespace panoptic_mapping {
 
 config_utilities::Factory::RegistrationRos<IDTrackerBase, SingleTSDFTracker,
@@ -13,18 +15,23 @@ config_utilities::Factory::RegistrationRos<IDTrackerBase, SingleTSDFTracker,
 
 void SingleTSDFTracker::Config::checkParams() const {
   checkParamConfig(submap);
+  checkParamConfig(renderer);
 }
 
 void SingleTSDFTracker::Config::setupParamsAndPrinting() {
   setupParam("verbosity", &verbosity);
   setupParam("submap", &submap);
   setupParam("use_detectron", &use_detectron);
-  setupParam("use_instance_classification", &use_instance_classification);
+  setupParam("use_detectron_panoptic", &use_detectron_panoptic);
+  setupParam("renderer", &renderer);
+  setupParam("min_new_instance_size", &min_new_instance_size);
 }
 
 SingleTSDFTracker::SingleTSDFTracker(const Config& config,
                                      std::shared_ptr<Globals> globals)
-    : config_(config.checkValid()), IDTrackerBase(std::move(globals)) {
+    : IDTrackerBase(std::move(globals)),
+      config_(config.checkValid()),
+      renderer_(config.renderer, globals_->camera()->getConfig(), false) {
   LOG_IF(INFO, config_.verbosity >= 1) << "\n" << config_.toString();
   addRequiredInput(InputData::InputType::kColorImage);
   addRequiredInput(InputData::InputType::kDepthImage);
@@ -47,8 +54,93 @@ void SingleTSDFTracker::processInput(SubmapCollection* submaps,
     setup(submaps);
   }
 
+  // Visualization
+  cv::Mat input_vis;
+  if (visualizationIsOn()) {
+    input_vis = renderer_.colorIdImage(input->idImage());
+  }
+
+  // Use only semantic information
   if (config_.use_detectron) {
     parseDetectronClasses(input);
+  }
+  // Use both semantic and instance level information
+  else if (config_.use_detectron_panoptic) {
+    // Convert the IDs in the ID image to panoptic ids
+    parseDetectronPanopticLabels(input);
+
+    int n_matched = 0;
+    int n_new = 0;
+
+    // Mapping from input ids to track ids i.e. existing instances
+    std::unordered_map<int, int> input_to_output;
+
+    // Render all the panoptic entities visible in the map
+    TrackingInfoAggregator tracking_data = computeTrackingData(submaps, input);
+
+    for (const int input_id : tracking_data.getInputIDs()) {
+      bool matched = false;
+      int matched_id = 0;
+      float score = 0.f;
+
+      // Compute IoU with all rendered segments
+      std::vector<std::pair<int, float>> ids_values;
+      bool any_overlap =
+          tracking_data.getAllMetrics(input_id, &ids_values, "iou");
+      if (!any_overlap) {
+        continue;
+      }
+
+      const auto input_class_id = input_id / kPanopticLabelDivisor_;
+
+      for (const auto& id_value : ids_values) {
+        // FIXME: this threshold should be either configurable or a constexpr!
+        if (id_value.second < 0.5) {
+          // No more matches possible as matches are ordered in decreasing order
+          break;
+        } else {
+          // Check the semantic class is the same
+          const auto class_id = id_value.first / kPanopticLabelDivisor_;
+          if (class_id != input_class_id) {
+            continue;
+          }
+
+          // Found the best match.
+          matched = true;
+          matched_id = id_value.first;
+          score = id_value.second;
+          break;
+        }
+      }
+
+      if (matched) {
+        ++n_matched;
+        input_to_output[input_id] = matched_id;
+      } else {
+        bool new_instance = tracking_data.getNumberOfInputPixels(input_id) >=
+                            config_.min_new_instance_size;
+        if (new_instance) {
+          input_to_output[input_id] = input_id;
+        } else {
+          input_to_output[input_id] = -1;
+        }
+      }
+    }
+
+    // Translate the id image - this will mostly affect thing classes`
+    for (auto it = input->idImagePtr()->begin<int>();
+         it != input->idImagePtr()->end<int>(); ++it) {
+      *it = input_to_output[*it];
+    }
+
+    // Publish Visualization if requested.
+    if (visualizationIsOn()) {
+      cv::Mat tracked_vis = renderer_.colorIdImage(input->idImage());
+      visualize(input_vis, "input");
+      visualize(rendered_vis_, "rendered");
+      visualize(input->colorImage(), "color");
+      visualize(tracked_vis, "tracked");
+    }
   }
 }
 
@@ -68,6 +160,30 @@ void SingleTSDFTracker::parseDetectronClasses(InputData* input) {
       *it = class_id;
     } else {
       *it = class_it->second;
+    }
+  }
+}
+
+void SingleTSDFTracker::parseDetectronPanopticLabels(InputData* input) {
+  std::unordered_map<int, int> detectron_to_panoptic_ids;
+  for (auto it = input->idImagePtr()->begin<int>();
+       it != input->idImagePtr()->end<int>(); ++it) {
+    if (*it == 0) {
+      continue;
+    }
+
+    auto panoptic_id_it = detectron_to_panoptic_ids.find(*it);
+    if (panoptic_id_it == detectron_to_panoptic_ids.end()) {
+      const auto& label = input->detectronLabels().at(*it);
+      int panoptic_id = label.category_id * kPanopticLabelDivisor_;
+      if (label.is_thing) {
+        // Instance ids should start from 1 so we reserve 0 for stuff classes
+        panoptic_id += label.instance_id + 1;
+      }
+      detectron_to_panoptic_ids[*it] = panoptic_id;
+      *it = panoptic_id;
+    } else {
+      *it = panoptic_id_it->second;
     }
   }
 }
@@ -94,6 +210,112 @@ void SingleTSDFTracker::setup(SubmapCollection* submaps) {
   }
   submaps->setActiveFreeSpaceSubmapID(map_id_);
   is_setup_ = true;
+}
+
+TrackingInfoAggregator SingleTSDFTracker::computeTrackingData(
+    SubmapCollection* submaps, InputData* input) {
+  TrackingInfoAggregator tracking_data;
+
+  // Process the input image
+  tracking_data.insertInputImage(input->idImage(), input->depthImage(),
+                                 globals_->camera()->getConfig(), 2);
+
+  // Render the single TSDF map - one tracking info object for
+  // each panoptic entity currently visible in the map
+  auto tracking_infos = renderTrackingInfoVertices(*(submaps->begin()), *input);
+
+  // Add tracking info to tracking data
+  tracking_data.insertTrackingInfos(tracking_infos);
+
+  // Render the data if required.
+  if (visualizationIsOn()) {
+    cv::Mat vis =
+        cv::Mat::ones(globals_->camera()->getConfig().height,
+                      globals_->camera()->getConfig().width, CV_32SC1) *
+        -1;
+    for (const TrackingInfo& info : tracking_infos) {
+      for (const Eigen::Vector2i& point : info.getPoints()) {
+        vis.at<int>(point.y(), point.x()) = info.getSubmapID();
+      }
+    }
+    rendered_vis_ = renderer_.colorIdImage(vis);
+  }
+
+  return tracking_data;
+}
+
+std::vector<TrackingInfo> SingleTSDFTracker::renderTrackingInfoVertices(
+    const Submap& submap, const InputData& input) {
+  std::unordered_map<int, TrackingInfo> results_table;
+
+  // Compute the maximum extent to lookup vertices.
+  const Transformation T_C_S = input.T_M_C().inverse() * submap.getT_M_S();
+  const Point origin_C = T_C_S * submap.getBoundingVolume().getCenter();
+  const Camera::Config& cam_config = globals_->camera()->getConfig();
+  std::vector<size_t> limits = {
+      0u /* x_min */, static_cast<size_t>(cam_config.width) /* x_max */,
+      0u /* y_min */, static_cast<size_t>(cam_config.height) /* y_max */};
+
+  size_t subsampling_factor = 1;
+  const Transformation T_S_C = T_C_S.inverse();
+  const TsdfLayer& tsdf_layer = submap.getTsdfLayer();
+
+  const float depth_tolerance =
+      config_.depth_tolerance > 0
+          ? config_.depth_tolerance
+          : -config_.depth_tolerance * submap.getTsdfLayer().voxel_size();
+  for (size_t u = limits[0]; u < limits[1];
+       u += config_.rendering_subsampling) {
+    for (size_t v = limits[2]; v < limits[3];
+         v += config_.rendering_subsampling) {
+      const float depth = input.depthImage().at<float>(v, u);
+      if (depth < cam_config.min_range || depth > cam_config.max_range) {
+        continue;
+      }
+      const cv::Vec3f& vertex = input.vertexMap().at<cv::Vec3f>(v, u);
+      const Point P_S = T_S_C * Point(vertex[0], vertex[1], vertex[2]);
+      const voxblox::BlockIndex block_index =
+          tsdf_layer.computeBlockIndexFromCoordinates(P_S);
+      const auto block_ptr = tsdf_layer.getBlockPtrByIndex(block_index);
+      if (block_ptr) {
+        const size_t voxel_index =
+            block_ptr->computeLinearIndexFromCoordinates(P_S);
+        const TsdfVoxel& voxel = block_ptr->getVoxelByLinearIndex(voxel_index);
+        if (voxel.weight < 1e-6 || std::abs(voxel.distance) > depth_tolerance) {
+          continue;
+        }
+
+        // Find or create a TrackingInfo for the panoptic entity of this voxel
+        const auto& voxel_panoptic_id =
+            submap
+                .getClassLayer()  // FIXME: assert that map has the class layer?
+                .getBlockConstPtrByIndex(block_index)
+                ->getVoxelByLinearIndex(voxel_index)
+                .getBelongingID();
+        auto it = results_table.find(voxel_panoptic_id);
+        if (it == results_table.end()) {
+          it = results_table
+                   .emplace(std::make_pair(voxel_panoptic_id,
+                                           TrackingInfo(voxel_panoptic_id)))
+                   .first;
+        }
+        TrackingInfo& result = it->second;
+
+        result.insertVertexPoint(input.idImage().at<int>(v, u));
+        if (visualizationIsOn()) {
+          result.insertVertexVisualizationPoint(u, v);
+        }
+      }
+    }
+  }
+
+  // Now move everything into an std::vector so we can directly pass
+  // this to TrackingInfoAggregator::inserTrackingInfos
+  std::vector<TrackingInfo> results_vector;
+  for (auto& id_result_pair : results_table) {
+    results_vector.push_back(std::move(id_result_pair.second));
+  }
+  return results_vector;
 }
 
 }  // namespace panoptic_mapping
