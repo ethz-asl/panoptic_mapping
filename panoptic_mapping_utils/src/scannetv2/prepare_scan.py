2740304d175e2356bca6eb61eb4cfca2355e6ff9
Giuliano Albanese <albanesg@student.ethz.ch>
2022-01-20 20:26:09 +0100

    Extend prepare scan to support deeplab labels

--- panoptic_mapping_utils/src/scannetv2/prepare_scan.py
+++ panoptic_mapping_utils/src/scannetv2/prepare_scan.py
@@ -1,9 +1,10 @@
 import argparse
 import csv
 import json
+import re
 import time
 from pathlib import Path
-from typing import List
+from typing import List, Optional
 from zipfile import ZipFile
 
 import numpy as np
@@ -13,17 +14,61 @@ from scannetv2_to_nyu40 import SCANNETV2_TO_NYU40
 
 _LABEL_DIR = "label-filt"
 _INSTANCE_DIR = "instance-filt"
-_DETECTRON_LABEL_DIR = "label-detectron"
+_LABEL_DETECTRON_DIR = "label-detectron"
 _NYU40_STUFF_CLASSES = [1, 2, 22]
 _DEFAULT_THING_SCORE = 0.9
 _TIMESTAMPS_FILE = "timestamps.csv"
 _FRAME_PERIOD_NS = 33333333
+_PANOPTIC_LABELS_CHOICES = ["groundtruth", "deeplab"]
+
+
+def get_scan_number_of_color_frames(scan_dir_path: Path):
+
+    scan_info_file_path = scan_dir_path / (scan_dir_path.stem + ".txt")
+    assert (
+        scan_info_file_path.exists()
+    ), f"Scene info file not found for {scan_dir_path.stem}"
+    with scan_info_file_path.open("r") as f:
+        for line in f:
+            if re.search("numColorFrames", line):
+                num_color_frames = int(line.rstrip("\n").split(" ")[2])
+                return num_color_frames
+
+
+def make_detectron_stuff_label(
+    segment_id,
+    class_id,
+    area,
+):
+    return {
+        "id": int(segment_id),
+        "isthing": False,
+        "category_id": int(class_id),
+        "area": int(area),
+    }
+
+
+def make_detectron_thing_label(
+    segment_id,
+    class_id,
+    instance_id,
+    area,
+    score=_DEFAULT_THING_SCORE,
+):
+    return {
+        "id": int(segment_id),
+        "isthing": True,
+        "category_id": int(class_id),
+        "instance_id": int(instance_id),
+        "area": int(area),
+        "score": float(score),
+    }
 
 
 def make_predicted_map_and_segment_labels(
     instance_map: np.ndarray,
     labels: np.ndarray,
-    stuff_classes: List[int],
+    instance_scores: List = None,
 ):
     predicted_map = np.zeros_like(instance_map)
     segment_labels = []
@@ -36,16 +81,11 @@ def make_predicted_map_and_segment_labels(
 
         class_mask = labels == class_id
 
-        if class_id in stuff_classes:
+        if class_id in _NYU40_STUFF_CLASSES:
             area = np.count_nonzero(class_mask)
             predicted_map[class_mask] = next_new_segment_id
             segment_labels.append(
-                {
-                    "id": next_new_segment_id,
-                    "isthing": False,
-                    "category_id": int(class_id),
-                    "area": int(area),
-                }
+                make_detectron_stuff_label(next_new_segment_id, class_id, area)
             )
             next_new_segment_id += 1
             continue
@@ -58,15 +98,26 @@ def make_predicted_map_and_segment_labels(
 
             predicted_map[instance_mask] = next_new_segment_id
 
+            if instance_scores is not None:
+                instance_score = next(
+                    (
+                        x["score"]
+                        for x in instance_scores
+                        if x["id"] == class_id and x["instance"] == instance_id
+                    ),
+                    0.0,
+                )
+            else:
+                instance_score = _DEFAULT_THING_SCORE
+
             segment_labels.append(
-                {
-                    "id": next_new_segment_id,
-                    "isthing": True,
-                    "score": _DEFAULT_THING_SCORE,
-                    "category_id": int(class_id),
-                    "instance_id": next_new_instance_id,
-                    "area": int(area),
-                }
+                make_detectron_thing_label(
+                    next_new_segment_id,
+                    class_id,
+                    next_new_instance_id,
+                    area,
+                    instance_score,
+                )
             )
 
             next_new_segment_id += 1
@@ -79,17 +130,10 @@ def convert_labels_to_nyu40(labels: np.ndarray):
     return np.vectorize(SCANNETV2_TO_NYU40.get)(labels).astype(np.uint8)
 
 
-def create_detectron_labels_from_groundtruth(
-    scan_dir_path: Path,
-    instance_dir: str,
-    label_dir: str,
-    out_dir: str,
-):
-    assert scan_dir_path.is_dir()
-
-    instance_dir_path = scan_dir_path / instance_dir
+def create_panoptic_labels_from_grountruth(scan_dir_path: Path):
+    instance_dir_path = scan_dir_path / _INSTANCE_DIR
     if not instance_dir_path.exists():
-        instance_archive_glob_pattern = f"*{instance_dir}.zip"
+        instance_archive_glob_pattern = f"*{_INSTANCE_DIR}.zip"
         # Try to extract the archive
         try:
             instance_archive_path = list(
@@ -101,10 +145,10 @@ def create_detectron_labels_from_groundtruth(
         instance_archive = ZipFile(str(instance_archive_path))
         instance_archive.extractall(path=str(scan_dir_path))
 
-    label_dir_path = scan_dir_path / label_dir
+    label_dir_path = scan_dir_path / _LABEL_DIR
     if not label_dir_path.exists():
         # Try to extract the archive
-        label_archive_glob_pattern = f"*{label_dir}.zip"
+        label_archive_glob_pattern = f"*{_LABEL_DIR}.zip"
         try:
             label_archive_path = list(scan_dir_path.glob(label_archive_glob_pattern))[0]
         except IndexError:
@@ -113,9 +157,8 @@ def create_detectron_labels_from_groundtruth(
         label_archive = ZipFile(str(label_archive_path))
         label_archive.extractall(path=str(scan_dir_path))
 
-    label_detectron_dir_path = scan_dir_path /out_dir
+    label_detectron_dir_path = scan_dir_path / _LABEL_DETECTRON_DIR
     label_detectron_dir_path.mkdir(exist_ok=True)
-    num_frames = 0
     for labels_file_path in label_dir_path.glob("*.png"):
         instance_file_path = instance_dir_path / labels_file_path.name
 
@@ -141,11 +184,50 @@ def create_detectron_labels_from_groundtruth(
         with segment_labels_file_path.open("w") as f:
             json.dump(segment_labels, f)
 
-        num_frames += 1
 
+def create_panoptic_labels_from_deeplab_predictions(
+    scan_dir_path: Path,
+    predicted_labels_dir: str,
+):
+    predicted_labels_dir_path = scan_dir_path / predicted_labels_dir
+    assert predicted_labels_dir_path.is_dir()
+
+    label_detectron_dir_path = scan_dir_path / _LABEL_DETECTRON_DIR
+    label_detectron_dir_path.mkdir(exist_ok=True)
+
+    for panoptic_map_file_path in predicted_labels_dir_path.glob("*.png"):
+        # Load the predicted panoptic map
+        panoptic_map = np.array(Image.open(panoptic_map_file_path))
+        semantic_map = panoptic_map[:, :, 0]
+        instance_map = panoptic_map[:, :, 1]
+
+        # Load the instance scores file
+        instance_scores_file_path = predicted_labels_dir_path / (
+            panoptic_map_file_path.name + "_instance_scores.json"
+        )
+        with open(instance_scores_file_path, "r") as j:
+            instance_scores = json.load(j)
+        predicted_map, segment_labels = make_predicted_map_and_segment_labels(
+            instance_map, semantic_map, instance_scores
+        )
+
+        predicted_map_file_path = label_detectron_dir_path / (
+            panoptic_map_file_path.stem + "_predicted.png"
+        )
+        Image.fromarray(predicted_map).save(predicted_map_file_path)
+
+        segment_labels_file_path = label_detectron_dir_path / (
+            panoptic_map_file_path.stem + "_labels.json"
+        )
+        with segment_labels_file_path.open("w") as f:
+            json.dump(segment_labels, f)
+
+
+def create_timestamps_file(scan_dir_path: Path):
     # Generate timestamps file
     next_frame_time = time.time_ns()
     timestamps_file_path = scan_dir_path / _TIMESTAMPS_FILE
+    num_frames = get_scan_number_of_color_frames(scan_dir_path)
     with open(timestamps_file_path, "w") as f:
         writer = csv.DictWriter(f, fieldnames=["FrameID", "TimeStamp"])
         writer.writeheader()
@@ -157,6 +239,29 @@ def create_detectron_labels_from_groundtruth(
                 }
             )
             next_frame_time += _FRAME_PERIOD_NS
+
+
+def prepare_scan(
+    scan_dir_path: Path,
+    panoptic_labels: str,
+    predicted_labels_dir: Optional[str],
+):
+    assert scan_dir_path.is_dir()
+
+    # Create timestamps file
+    create_timestamps_file(scan_dir_path)
+
+    # Create panoptic labels
+    if panoptic_labels == "groundtruth":
+        create_panoptic_labels_from_grountruth(scan_dir_path)
+    else:
+        assert (
+            predicted_labels_dir is not None
+        ), "The name of directory containing predicted labels must be provided"
+        create_panoptic_labels_from_deeplab_predictions(
+            scan_dir_path,
+            predicted_labels_dir,
+        )
 
 
 def _parse_args():
@@ -171,24 +276,18 @@ def _parse_args():
     )
 
     parser.add_argument(
-        "--instance-dir",
+        "panoptic_labels",
         type=str,
-        default=_INSTANCE_DIR,
-        help="Name of the subdirectory containing instance maps.",
+        choices=_PANOPTIC_LABELS_CHOICES,
+        default="groundtruth",
+        help="How the labels should be generated",
     )
 
     parser.add_argument(
-        "--label-dir",
+        "predicted_labels_dir",
         type=str,
-        default=_LABEL_DIR,
-        help="Name of the subdirectory containing semantic labels.",
-    )
-
-    parser.add_argument(
-        "--out-dir",
-        type=str,
-        default=_DETECTRON_LABEL_DIR,
-        help="Name of the subdirectory where the generated labels will be saved.",
+        default=None,
+        help="Name of the subdirectory containing the predicted labels.",
     )
 
     return parser.parse_args()
@@ -196,9 +295,8 @@ def _parse_args():
 
 if __name__ == "__main__":
     args = _parse_args()
-    create_detectron_labels_from_groundtruth(
+    prepare_scan(
         scan_dir_path=args.scan_dir_path,
-        instance_dir=args.instance_dir,
-        label_dir=args.label_dir,
-        out_dir=args.out_dir,
+        panoptic_labels=args.panoptic_labels,
+        deeplab_labels_dir=args.predicted_labels_dir,
     )
