076bb53f4978d3e1e5cb20fa940951abe9fc598b
Giuliano Albanese <albanesg@student.ethz.ch>
2022-02-05 18:36:12 +0100

    Refactor scannetv2 data player

--- panoptic_mapping_utils/src/scannetv2/scannetv2_data_player.py
+++ panoptic_mapping_utils/src/scannetv2/scannetv2_data_player.py
@@ -4,17 +4,16 @@ import json
 import csv
 from dataclasses import dataclass
 from pathlib import Path
-from tkinter import Frame
-from typing import List, Dict, Optional
+from typing import List, Dict, Tuple, Optional
 
 import cv2
 import numpy as np
-from phonenumbers import format_number_for_mobile_dialing
 import rospy
 import tf
 from cv_bridge import CvBridge
 from geometry_msgs.msg import PoseStamped, Point, Quaternion
 from PIL import Image as PilImage
+from std_msgs.msg import Header
 from sensor_msgs.msg import Image
 
 from std_srvs.srv import Empty, EmptyResponse
@@ -27,7 +26,7 @@ _PANOPTIC_GROUNDTRUTH_DIR = "panoptic"
 _PANOPTIC_PRED_DIR = "panoptic_pred"
 _POSES_DIR = "pose"
 _DEPTH_SHIFT = 1000
-_INPUT_IMAGE_DIMS = (320, 250)
+_INPUT_IMAGE_DIMS = (320, 240)
 
 
 @dataclass
@@ -40,30 +39,28 @@ class FrameData:
     uncertainty: Optional[np.ndarray] = None
 
 
-class ScannetV2DataPlayer:
-    def __init__(self):
+def make_msg_header(self, timestamp, frame_id: str):
+    return Header(
+        stamp=timestamp,
+        frame_id=frame_id,
+    )
 
-        # node params
-        self.scans_dir_path = Path(rospy.get_param("~scans_dir_path"))
-        self.scan_id = rospy.get_param("~scan_id")
-        self.scan_dir_path = self.scans_dir_path / self.scan_id
-        if not self.scan_dir_path.exists():
-            raise RuntimeError("Invalid scan dir path!")
 
-        self.global_frame_name = rospy.get_param("~global_frame_name", "world")
-        self.sensor_frame_name = rospy.get_param("~sensor_frame_name", "depth_cam")
-        self.play_rate = rospy.get_param("~play_rate", 1.0)
-        self.wait = rospy.get_param("~wait", False)
-        self.frame_skip = rospy.get_param("~frame_skip", 0)
-        self.refresh_rate = 30  # Hz
-        self.use_uncertainty = rospy.get_param("~use_uncertainty", False)
+class FrameDataPublisher:
+    def __init__(
+        self,
+        sensor_frame_name: str,
+        global_frame_name: str,
+        use_uncertainty=False,
+    ):
+        self.sensor_frame_name = sensor_frame_name
+        self.global_frame_name = global_frame_name
 
-        self.use_groundtruth = rospy.get_param("~use_groundtruth", False)
-
-        # Configure sensor data publishers
+        self.cv_bridge = CvBridge()
         self.color_pub = rospy.Publisher("~color_image", Image, queue_size=100)
         self.depth_pub = rospy.Publisher("~depth_image", Image, queue_size=100)
         self.id_pub = rospy.Publisher("~id_image", Image, queue_size=100)
+        self.use_uncertainty = use_uncertainty
         if self.use_uncertainty:
             self.uncertainty_pub = rospy.Publisher(
                 "~uncertainty_image", Image, queue_size=100
@@ -72,49 +69,119 @@ class ScannetV2DataPlayer:
         self.pose_pub = rospy.Publisher("~pose", PoseStamped, queue_size=100)
         self.tf_broadcaster = tf.TransformBroadcaster()
 
-        # Setup
-        self.cv_bridge = CvBridge()
+    def publish(self, frame_data: FrameData, timestamp):
+        # Color
+        color_img_msg = self.cv_bridge.cv2_to_imgmsg(frame_data.color, "bgr8")
+        color_img_msg.header = make_msg_header(timestamp, self.sensor_frame_name)
+        self.color_pub.publish(color_img_msg)
 
-        self.current_index = 0
-        self.times = []
-        self.ids = []
-        timestamps_file_path = self.scan_dir_path / "timestamps.csv"
-        if not timestamps_file_path.is_file():
-            rospy.logfatal("Timestamps file not found!")
+        # Depth
+        depth_img_msg = self.cv_bridge.cv2_to_imgmsg(frame_data.depth, "32FC1")
+        depth_img_msg.header = make_msg_header(timestamp, self.sensor_frame_name)
+        self.depth_pub.publish(depth_img_msg)
+
+        # Segmentation
+        id_img_msg = self.cv_bridge.cv2_to_imgmsg(frame_data.segmentation, "32SC1")
+        id_img_msg.header = make_msg_header(timestamp, self.sensor_frame_name)
+        self.id_pub.publish(id_img_msg)
+
+        # Segments info (Detectron labels)
+        label_msg = DetectronLabels()
+        label_msg.header = make_msg_header(timestamp, self.sensor_frame_name)
+        for d in frame_data.segments_info:
+            if "instance_id" not in d:
+                d["instance_id"] = 0
+            if "score" not in d:
+                d["score"] = 0
+            label = DetectronLabel()
+            label.id = d["id"]
+            label.instance_id = d["instance_id"]
+            label.is_thing = d["isthing"]
+            label.category_id = d["category_id"]
+            label.score = d["score"]
+            label_msg.labels.append(label)
+        self.label_pub.publish(label_msg)
+
+        # Transform and pose
+        rotation = tf.transformations.quaternion_from_matrix(frame_data.pose)
+        translation = tuple(frame_data.pose[0:3, -1])
+
+        self.tf_broadcaster.sendTransform(
+            translation=translation,
+            rotation=rotation,
+            time=timestamp,
+            child=self.sensor_frame_name,
+            parent=self.global_frame_name,
+        )
+
+        pose_msg = PoseStamped()
+        pose_msg.header = make_msg_header(timestamp, self.global_frame_name)
+        pose_msg.pose.position = Point(
+            **{
+                "x": translation[0],
+                "y": translation[1],
+                "z": translation[2],
+            }
+        )
+        pose_msg.pose.orientation = Quaternion(
+            **{
+                "x": rotation[0],
+                "y": rotation[1],
+                "z": rotation[2],
+                "w": rotation[3],
+            }
+        )
+        self.pose_pub.publish(pose_msg)
+
+        # Dense semantic uncertainty
+        if self.use_uncertainty:
+            uncertainty_img_msg = self.cv_bridge.cv2_to_imgmsg(
+                frame_data.uncertainty, "32FC1"
+            )
+            uncertainty_img_msg.header.stamp = timestamp
+            uncertainty_img_msg.header.frame_id = self.sensor_frame_name
+            self.uncertainty_pub.publish(uncertainty_img_msg)
+
+
+class FrameDataLoader:
+    def __init__(
+        self,
+        scan_dir_path: Path,
+        image_size: Tuple[int, int] = _INPUT_IMAGE_DIMS,
+        use_uncertainty: bool = False,
+    ):
+        self.scan_dir_path = scan_dir_path
+        self.image_size = image_size
+        self.use_uncertainty = use_uncertainty
+
+    @staticmethod
+    def load_frame_ids_and_timestamps(
+        timestamps_file_path: Path,
+        play_rate: int,
+        frame_skip: int = 0,
+    ):
+        ids = []
+        times = []
         with timestamps_file_path.open("r") as f:
             csv_reader = csv.reader(f)
             for row in csv_reader:
                 if row[0] == "FrameID":
                     continue
-                self.ids.append(str(row[0]))
-                self.times.append(float(row[1]) / 1e9)
+                ids.append(str(row[0]))
+                times.append(float(row[1]) / 1e9)
 
-        self.ids = [x for _, x in sorted(zip(self.times, self.ids))]
-        self.times = sorted(self.times)
+        ids = [x for _, x in sorted(zip(times, ids))]
+        times = sorted(times)
 
         # Drop frames
-        if self.frame_skip > 0:
-            self.ids = [
-                x for idx, x in enumerate(self.ids) if idx % self.frame_skip == 0
-            ]
-            self.times = [
-                x for idx, x in enumerate(self.times) if idx % self.frame_skip == 0
-            ]
+        if frame_skip > 0:
+            ids = [x for idx, x in enumerate(ids) if idx % frame_skip == 0]
+            times = [x for idx, x in enumerate(times) if idx % frame_skip == 0]
 
-        self.times = [(x - self.times[0]) / self.play_rate for x in self.times]
-        self.start_time = None
+        times = [(x - times[0]) / play_rate for x in times]
+        return ids, times
 
-        if self.wait:
-            self.start_srv = rospy.Service("~start", Empty, self.start)
-        else:
-            self.start(None)
-
-    def start(self, _):
-        self.running = True
-        self.timer = rospy.Timer(rospy.Duration(1.0 / self.refresh_rate), self.callback)
-        return EmptyResponse
-
-    def _load_frame_data(self, frame_id) -> FrameData:
+    def load(self, frame_id) -> FrameData:
         pose_file_path = (
             self.scan_dir_path / _POSES_DIR / "{:05d}.txt".format(int(frame_id))
         )
@@ -180,82 +247,60 @@ class ScannetV2DataPlayer:
 
         return frame_data
 
-    def _publish_frame_data(self, frame_data: FrameData, timestamp):
-        # Color
-        color_img_msg = self.cv_bridge.cv2_to_imgmsg(frame_data.color, "bgr8")
-        color_img_msg.header.stamp = timestamp
-        color_img_msg.header.frame_id = self.sensor_frame_name
-        self.color_pub.publish(color_img_msg)
 
-        # Depth
-        depth_img_msg = self.cv_bridge.cv2_to_imgmsg(frame_data.depth, "32FC1")
-        depth_img_msg.header.stamp = timestamp
-        depth_img_msg.header.frame_id = self.sensor_frame_name
-        self.depth_pub.publish(depth_img_msg)
+class ScannetV2DataPlayer:
+    def __init__(self):
 
-        # Segmentation
-        id_img_msg = self.cv_bridge.cv2_to_imgmsg(frame_data.segmentation, "32SC1")
-        id_img_msg.header.stamp = timestamp
-        id_img_msg.header.frame_id = self.sensor_frame_name
-        self.id_pub.publish(id_img_msg)
+        # node params
+        self.scans_dir_path = Path(rospy.get_param("~scans_dir_path"))
+        self.scan_id = rospy.get_param("~scan_id")
+        self.scan_dir_path = self.scans_dir_path / self.scan_id
+        if not self.scan_dir_path.is_dir():
+            rospy.logfatal(f"{str(self.scan_dir_path)} is not a valid directory path!")
 
-        # Segments info (Detectron labels)
-        label_msg = DetectronLabels()
-        label_msg.header.stamp = timestamp
-        for d in frame_data.segments_info:
-            if "instance_id" not in d:
-                d["instance_id"] = 0
-            if "score" not in d:
-                d["score"] = 0
-            label = DetectronLabel()
-            label.id = d["id"]
-            label.instance_id = d["instance_id"]
-            label.is_thing = d["isthing"]
-            label.category_id = d["category_id"]
-            label.score = d["score"]
-            label_msg.labels.append(label)
-        self.label_pub.publish(label_msg)
+        self.global_frame_name = rospy.get_param("~global_frame_name", "world")
+        self.sensor_frame_name = rospy.get_param("~sensor_frame_name", "depth_cam")
+        self.play_rate = rospy.get_param("~play_rate", 1.0)
+        self.wait = rospy.get_param("~wait", False)
+        self.frame_skip = rospy.get_param("~frame_skip", 0)
+        self.refresh_rate = 30  # Hz
+        self.use_uncertainty = rospy.get_param("~use_uncertainty", False)
 
-        # Pose
-        rotation = tf.transformations.quaternion_from_matrix(frame_data.pose)
-        translation = tuple(frame_data.pose[0:3, -1])
-
-        self.tf_broadcaster.sendTransform(
-            translation=translation,
-            rotation=rotation,
-            time=timestamp,
-            child=self.sensor_frame_name,
-            parent=self.global_frame_name,
+        # Configure frame data loader
+        self.frame_data_loader = FrameDataLoader(
+            self.scan_dir_path,
+            _INPUT_IMAGE_DIMS,
+            self.use_uncertainty,
         )
 
-        pose_msg = PoseStamped()
-        pose_msg.header.stamp = timestamp
-        pose_msg.header.frame_id = self.global_frame_name
-        pose_msg.pose.position = Point(
-            **{
-                "x": translation[0],
-                "y": translation[1],
-                "z": translation[2],
-            }
+        # Configure frame data publisher
+        self.frame_data_publisher = FrameDataPublisher(
+            self.sensor_frame_name,
+            self.global_frame_name,
+            self.use_uncertainty,
         )
-        pose_msg.pose.orientation = Quaternion(
-            **{
-                "x": rotation[0],
-                "y": rotation[1],
-                "z": rotation[2],
-                "w": rotation[3],
-            }
-        )
-        self.pose_pub.publish(pose_msg)
 
-        # Dense semantic uncertainty
-        if self.use_uncertainty:
-            uncertainty_img_msg = self.cv_bridge.cv2_to_imgmsg(
-                frame_data.uncertainty, "32FC1"
-            )
-            uncertainty_img_msg.header.stamp = timestamp
-            uncertainty_img_msg.header.frame_id = self.sensor_frame_name
-            self.uncertainty_pub.publish(uncertainty_img_msg)
+        self.current_index = 0
+        self.start_time = None
+
+        timestamps_file_path = self.scan_dir_path / "timestamps.csv"
+        if not timestamps_file_path.is_file():
+            rospy.logfatal("Timestamps file not found!")
+        self.ids, self.times = FrameDataLoader.load_frame_ids_and_timestamps(
+            timestamps_file_path,
+            self.play_rate,
+            self.frame_skip,
+        )
+
+        if self.wait:
+            self.start_srv = rospy.Service("~start", Empty, self.start)
+        else:
+            self.start(None)
+
+    def start(self, _):
+        self.running = True
+        self.timer = rospy.Timer(rospy.Duration(1.0 / self.refresh_rate), self.callback)
+        return EmptyResponse
 
     def callback(self, _):
 
@@ -272,10 +317,10 @@ class ScannetV2DataPlayer:
         frame_id = self.ids[self.current_index]
 
         # Load and publish next frame data
-        frame_data = self._load_frame_data(frame_id)
+        frame_data = self.frame_data_loader.load(frame_id)
         if frame_data is not None:
-            rospy.loginfo(f"Publishing data for frame {frame_id}")
-            self._publish_frame_data(frame_data, now)
+            rospy.logdebug(f"Publishing data for frame {frame_id}")
+            self.frame_data_publisher.publish(frame_data, now)
         else:
             rospy.logwarn(f"Data for frame {frame_id} could not be loaded. Skipped.")
 
