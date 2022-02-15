7f4404d7d53a22fdd9eceedead81304034dfad1b
Hermann <blumh@ethz.ch>
2022-02-15 19:37:26 +0100

    adapt scannet player

--- panoptic_mapping_utils/src/scannetv2/scannetv2_data_player.py
+++ panoptic_mapping_utils/src/scannetv2/scannetv2_data_player.py
@@ -12,65 +12,44 @@ import rospy
 import tf
 from cv_bridge import CvBridge
 from geometry_msgs.msg import PoseStamped, Point, Quaternion
-from PIL import Image as PilImage
 from sensor_msgs.msg import Image
 
 from std_srvs.srv import Empty, EmptyResponse
 from panoptic_mapping_msgs.msg import DetectronLabel, DetectronLabels
 
-_COLOR_IMAGES_DIR = "color"
-_DEPTH_IMAGES_DIR = "depth"
-_LABEL_DIR = "label-detectron"
-_POSES_DIR = "pose"
-_DEPTH_SHIFT = 1000
-
 
 class ScannetV2DataPlayer:
-    def __init__(self):
 
+    def __init__(self):
         # node params
         self.data_dir_path = Path(rospy.get_param("~data_path"))
         if not self.data_dir_path.exists():
-            raise RuntimeError("Invalid data dir path!")
+            raise RuntimeError(f"Invalid data dir path: {self.data_dir_path}")
+        self.inference_dir_path = Path(rospy.get_param("~inference_path"))
+        if not self.inference_dir_path.exists():
+            raise RuntimeError(f"Invalid inference dir path: {self.inference_dir_path}")
+        self.id_fileending = rospy.get_param("~id_fileending")
+        self.id_filestart = '_'.join(
+            os.listdir(self.inference_dir_path)[0].split('_')[:2])
 
         self.global_frame_name = rospy.get_param("~global_frame_name", "world")
-        self.sensor_frame_name = rospy.get_param("~sensor_frame_name", "depth_cam")
-        self.play_rate = rospy.get_param("~play_rate", 1.0)
-        self.use_predicted_labels = rospy.get_param("~use_predicted_labels", False)
+        self.sensor_frame_name = rospy.get_param("~sensor_frame_name",
+                                                 "depth_cam")
+        self.play_rate = rospy.get_param("~play_rate", 1.0 / 30)
+        self.use_predicted_labels = rospy.get_param("~use_predicted_labels",
+                                                    False)
         self.wait = rospy.get_param("~wait", False)
-        self.refresh_rate = 30  # Hz
-
-        self.wait = rospy.get_param("~wait")
 
         # Configure sensor data publishers
         self.color_pub = rospy.Publisher("~color_image", Image, queue_size=100)
         self.depth_pub = rospy.Publisher("~depth_image", Image, queue_size=100)
         self.id_pub = rospy.Publisher("~id_image", Image, queue_size=100)
-        self.label_pub = rospy.Publisher("~labels", DetectronLabels, queue_size=100)
-        if self.use_predicted_labels:
-            raise NotImplementedError
         self.pose_pub = rospy.Publisher("~pub", PoseStamped, queue_size=100)
         self.tf_broadcaster = tf.TransformBroadcaster()
 
         # Setup
         self.cv_bridge = CvBridge()
         self.current_index = 0
-        self.times = []
-        self.ids = []
-        timestamps_file_path = self.data_dir_path / "timestamps.csv"
-        if not timestamps_file_path.is_file():
-            rospy.logfatal("Timestamps file not found!")
-        with timestamps_file_path.open("r") as f:
-            csv_reader = csv.reader(f)
-            for row in csv_reader:
-                if row[0] == "FrameID":
-                    continue
-                self.ids.append(str(row[0]))
-                self.times.append(float(row[1]) / 1e9)
-
-        self.ids = [x for _, x in sorted(zip(self.times, self.ids))]
-        self.times = sorted(self.times)
-        self.times = [(x - self.times[0]) / self.play_rate for x in self.times]
         self.start_time = None
 
         if self.wait:
@@ -80,7 +59,7 @@ class ScannetV2DataPlayer:
 
     def start(self, _):
         self.running = True
-        self.timer = rospy.Timer(rospy.Duration(1.0 / self.refresh_rate), self.callback)
+        self.timer = rospy.Timer(rospy.Duration(self.play_rate), self.callback)
         return EmptyResponse
 
     def _load_and_publish_color(self, color_image_file_path, timestamp):
@@ -91,40 +70,25 @@ class ScannetV2DataPlayer:
         self.color_pub.publish(img_msg)
 
     def _load_and_publish_depth(self, depth_image_file_path, timestamp):
-        raw_depth = np.array(PilImage.open(str(depth_image_file_path)))
+        raw_depth = cv2.imread(str(depth_image_file_path),
+                               cv2.IMREAD_UNCHANGED)
         # Convert depth to meters
-        depth = raw_depth.astype(np.float32) / _DEPTH_SHIFT
+        depth = raw_depth.astype(np.float32) / 1000
         img_msg = self.cv_bridge.cv2_to_imgmsg(depth, "32FC1")
         img_msg.header.stamp = timestamp
         img_msg.header.frame_id = self.sensor_frame_name
         self.depth_pub.publish(img_msg)
 
-    def _load_and_publish_instance(self, instance_file_path, timestamp):
+    def _load_and_publish_id(self, id_filepath, timestamp):
         # Load instance and publish instance
-        instance_map = np.array(PilImage.open(instance_file_path))
-        img_msg = self.cv_bridge.cv2_to_imgmsg(instance_map, "8UC1")
+        id_map = np.load(str(id_filepath))
+        if id_map.dtype != 'uint8':
+            rospy.logwarn(f"ID is of type {id_map.dtype}, converting to uint8.")
+            id_map = id_map.astype('uint8')
+        img_msg = self.cv_bridge.cv2_to_imgmsg(id_map, "8UC1")
         img_msg.header.stamp = timestamp
         img_msg.header.frame_id = self.sensor_frame_name
         self.id_pub.publish(img_msg)
-
-    def _load_and_publish_labels(self, label_file_path, timestamp):
-        label_msg = DetectronLabels()
-        label_msg.header.stamp = timestamp
-        with label_file_path.open("r") as f:
-            data = json.load(f)
-            for d in data:
-                if "instance_id" not in d:
-                    d["instance_id"] = 0
-                if "score" not in d:
-                    d["score"] = 0
-                label = DetectronLabel()
-                label.id = d["id"]
-                label.instance_id = d["instance_id"]
-                label.is_thing = d["isthing"]
-                label.category_id = d["category_id"]
-                label.score = d["score"]
-                label_msg.labels.append(label)
-        self.label_pub.publish(label_msg)
 
     def _load_and_publish_pose(self, pose_file_path, timestamp):
         # Load the transformation matrix from a text file
@@ -144,67 +108,48 @@ class ScannetV2DataPlayer:
         pose_msg = PoseStamped()
         pose_msg.header.stamp = timestamp
         pose_msg.header.frame_id = self.global_frame_name
-        pose_msg.pose.position = Point(
-            **{
-                "x": translation[0],
-                "y": translation[1],
-                "z": translation[2],
-            }
-        )
+        pose_msg.pose.position = Point(**{
+            "x": translation[0],
+            "y": translation[1],
+            "z": translation[2],
+        })
         pose_msg.pose.orientation = Quaternion(
             **{
                 "x": rotation[0],
                 "y": rotation[1],
                 "z": rotation[2],
                 "w": rotation[3],
-            }
-        )
+            })
         self.pose_pub.publish(pose_msg)
 
     def callback(self, _):
-
         if not self.running:
             return
-
         now = rospy.Time.now()
         if self.start_time is None:
             self.start_time = now
-
-        if self.times[self.current_index] > (now - self.start_time).to_sec():
-            return
-
         # Load and publish the next image
-        frame_id = self.ids[self.current_index]
-
-        color_image_file_path = (
-            self.data_dir_path / _COLOR_IMAGES_DIR / "{:05d}.jpg".format(int(frame_id))
-        )
-        depth_image_file_path = (
-            self.data_dir_path / _DEPTH_IMAGES_DIR / "{:05d}.png".format(int(frame_id))
-        )
-        pose_file_path = (
-            self.data_dir_path / _POSES_DIR / "{:05d}.txt".format(int(frame_id))
-        )
-        instance_map_file_path = (
-            self.data_dir_path / _LABEL_DIR / f"{frame_id}_predicted.png"
-        )
-        labels_file_path = self.data_dir_path / _LABEL_DIR / f"{frame_id}_labels.json"
-
-        for f in [color_image_file_path, depth_image_file_path, pose_file_path]:
-            if not f.is_file():
-                rospy.logwarn(f"Could not find file '{str(f)}', skipping frame.")
-                self.current_index += 1
-                return
-
-        self._load_and_publish_color(color_image_file_path, now)
-        self._load_and_publish_instance(instance_map_file_path, now)
-        self._load_and_publish_labels(labels_file_path, now)
-        self._load_and_publish_depth(depth_image_file_path, now)
-        self._load_and_publish_pose(pose_file_path, now)
-
         self.current_index += 1
-        if self.current_index >= len(self.ids):
-            rospy.signal_shutdown("Player reached end of sequence.")
+        color_filepath = self.data_dir_path / "color" / f"{self.current_index:d}.jpg"
+        depth_filepath = self.data_dir_path / "depth" / f"{self.current_index:d}.png"
+        pose_filepath = self.data_dir_path / "pose" / f"{self.current_index:d}.txt"
+        idmap_filepath = (
+            self.inference_dir_path /
+            f"{self.id_filestart}_{self.current_index:06d}_{self.id_fileending}.npy"
+        )
+        for f in [
+                color_filepath, depth_filepath, pose_filepath, idmap_filepath
+        ]:
+            if not f.is_file():
+                rospy.logwarn(
+                    f"Could not find file '{str(f)}', shutting down.")
+                rospy.sleep(5)
+                rospy.signal_shutdown("Player reached end of sequence.")
+                return
+        self._load_and_publish_color(color_filepath, now)
+        self._load_and_publish_id(idmap_filepath, now)
+        self._load_and_publish_depth(depth_filepath, now)
+        self._load_and_publish_pose(pose_filepath, now)
 
 
 if __name__ == "__main__":
